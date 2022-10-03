0b1581b4efcfa644d55af4145da6a8db9607e5d4
shungo0222 <kimura.shungo@gmail.com>
2022-10-03 19:05:26 +0900

    create the UI for taking pictures screen

---
+++ lib/screen/album_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AlbumScreen extends StatelessWidget {
+  const AlbumScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
+      body: const Center(
+        child: Text('Album Screen'),
+      ),
+    );
+  }
+}
---
+++ lib/screen/take_picture_screen.dart
@@ -0,0 +1,93 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'album_screen.dart';
+import 'display_picture_screen.dart';
+
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  final CameraDescription camera;
+
+  @override
+  State<TakePictureScreen> createState() => _TakePictureScreenState();
+}
+
+class _TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.max,
+    );
+    _initializeControllerFuture = _controller.initialize();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: FutureBuilder<void>(
+              future: _initializeControllerFuture,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return CameraPreview(_controller);
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+          Positioned(
+            bottom: 55,
+            right: 30,
+            child: IconButton(
+              icon: const Icon(
+                Icons.photo_album,
+                color: Colors.green,
+              ),
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => const AlbumScreen(),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromRGBO(0, 0, 0, 0.5),
+        onPressed: () async {
+          final image = await _controller.takePicture();
+          await Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => DisplayPictureScreen(imagePath: image.path),
+            ),
+          );
+        },
+        child: const Icon(
+          Icons.camera_alt,
+          color: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
